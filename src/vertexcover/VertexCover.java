d80cb90e389de016d02b8cc5db68c782e88e902b
Pawel Kowalewski <pawelowsky@gmail.com>
2019-04-04 10:38:42 +0200

    Added Vertex cover finding algorithm

--- src/vertexcover/VertexCover.java
+++ src/vertexcover/VertexCover.java
@@ -1,5 +1,10 @@
 package vertexcover;
 
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 /**
  *
  * @author komabjn
@@ -36,6 +41,73 @@ public class VertexCover {
             System.out.println("failed to load matrix from binary file at: " + GRAPH_FILEPATH);
             return;
         }
-        //TODO logic of finding vertex cover
+
+        boolean visitedVertices[] = new boolean[g.getNumberOfVertices()];
+        LinkedList<Integer> adjacentVertices[] = (LinkedList<Integer>[]) new LinkedList[g.getNumberOfVertices()];
+        Deque<Integer> lastVisitedVertices = new LinkedList<>();
+
+        int lastStartingVertex = -1;
+        int currentVertex = 0;
+        boolean didFindNotVisitedVertexInCurrentVertex;
+
+        Set<Integer> vertexCover = new HashSet<>();
+        for (int i = 0; i < g.getNumberOfVertices(); i++) {
+            vertexCover.add(i);
+        }
+
+        while (true) {
+            //find starting point in graph (from vertices which werent yet visited)
+            didFindNotVisitedVertexInCurrentVertex = false;
+            for (int i = lastStartingVertex + 1; i < g.getNumberOfVertices(); i++) {
+                if (!visitedVertices[i]) {
+                    lastStartingVertex = i;
+                    currentVertex = i;
+                    visitedVertices[i] = true;
+                    adjacentVertices[i] = g.getAdjacentVertices(i);
+                    lastVisitedVertices.addFirst(i);
+                    didFindNotVisitedVertexInCurrentVertex = true;
+                    break;
+                }
+            }
+            if (!didFindNotVisitedVertexInCurrentVertex) {
+                break;
+            }
+
+            //perform DFS
+            boolean didMoveToDeeperVertex = false;
+
+            while (!lastVisitedVertices.isEmpty()) {
+                currentVertex = lastVisitedVertices.getFirst();
+                for (int i = 0; i < adjacentVertices[currentVertex].size(); i++) {
+                    if (visitedVertices[adjacentVertices[currentVertex].getFirst()]) {//if vertex visited
+                        adjacentVertices[currentVertex].removeFirst();
+                    } else {
+                        currentVertex = adjacentVertices[currentVertex].getFirst();
+                        lastVisitedVertices.addFirst(currentVertex);
+                        visitedVertices[currentVertex] = true;
+                        adjacentVertices[currentVertex] = g.getAdjacentVertices(currentVertex);
+                        didMoveToDeeperVertex = true;
+                        break;
+                    }
+                }
+                if (adjacentVertices[currentVertex].isEmpty()) { //no more vertices
+                    if (didMoveToDeeperVertex) { //no vertices to visit and moved deeper, we are in the leaf
+                        vertexCover.remove(currentVertex);
+                    }
+                    lastVisitedVertices.removeFirst();
+                    adjacentVertices[currentVertex] = null;
+
+                    didMoveToDeeperVertex = false;
+                    //break;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Found vertex cover: \n");
+        for (Integer i : vertexCover) {
+            sb.append(i.toString());
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
